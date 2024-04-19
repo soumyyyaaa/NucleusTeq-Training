db9b753a3cb985899a171ed8df36133e4490feae
soumyyyaaa <soumya.agarwal1103@gmail.com>
2024-04-19 22:49:43 +0530

    API Assignment Changed

--- API Assignment/script.js
+++ API Assignment/script.js
@@ -1,22 +1,51 @@
 function fetchData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then((response) => response.json())
-        .then((data) => {
-            const container = document.getElementById("card");
-
-            data.forEach((item) => {
-                const div = document.createElement("div");
-                const h4 = document.createElement("h4");
-                const p = document.createElement("p");
-                h4.textContent = item.title;
-                p.textContent = item.body;
-                div.appendChild(h4);
-                div.appendChild(p);
-                container.appendChild(div);
-                div.classList.add("card1");
-                h4.classList.add("center");
-                p.classList.add("justify");
+        .then((posts) => {
+            posts.sort((a, b) => a.userId - b.userId);
+            const postsByUser = {};
+            const grid = document.getElementById("grid");
+            posts.forEach((post) => {
+                if (!postsByUser[post.userId]) {
+                    postsByUser[post.userId] = [];
+                }
+                postsByUser[post.userId].push(post);
             });
+
+            for (const userId in postsByUser) {
+                const mainDiv = document.createElement("div");
+                const carouselInner = document.createElement("div");
+                var isFirstPost = true;
+                postsByUser[userId].forEach((post) => {
+                    const carouselItem = document.createElement("div");
+                    const h4 = document.createElement("h4");
+                    const h5 = document.createElement("h5");
+                    const p = document.createElement("p");
+                    h4.textContent = `User Id: ${post.userId}`;
+                    h5.textContent = post.title;
+                    p.textContent = post.body;
+                    h4.classList.add("center");
+                    h5.classList.add("center");
+                    p.classList.add("justify");
+                    carouselItem.appendChild(h4);
+                    carouselItem.appendChild(h5);
+                    carouselItem.appendChild(p);
+                    carouselInner.appendChild(carouselItem);
+                    carouselInner.setAttribute("class", "carousel-inner");
+                    if (isFirstPost) {
+                        carouselItem.classList.add("carousel-item", "active");
+                        isFirstPost = false;
+                    } else {
+                        carouselItem.classList.add("carousel-item");
+                    }
+                });
+                mainDiv.appendChild(carouselInner);
+                mainDiv.setAttribute("id", "carouselExampleSlidesOnly");
+                mainDiv.classList.add("carousel", "slide");
+                mainDiv.setAttribute("data-bs-ride", "carousel");
+                grid.appendChild(mainDiv);
+            }
+            $(".carousel").carousel();
         })
         .catch((error) => {
             console.error("Error:", error);
