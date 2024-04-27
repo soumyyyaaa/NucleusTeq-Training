6d47f4811a95c1d2a178fd9df9c3b495ec0d5ae1
soumyyyaaa <soumya.agarwal1103@gmail.com>
2024-04-28 02:08:41 +0530

    Category Selection Page Added

--- Interactive Quiz Assignment/script.js
+++ Interactive Quiz Assignment/script.js
@@ -3,15 +3,51 @@ function startButtonCLick() {
     document.getElementById("difficulty-level-page").style.display = "block";
 }
 
-//-----for setting difficulty------
+let selected_difficulty;
+let selected_category;
+
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".difficulty-btn");
     buttons.forEach((button) => {
         button.addEventListener("mousedown", function () {
-            const difficultyLevel = this.getAttribute("value");
-            console.log(difficultyLevel);
+            selected_difficulty = this.getAttribute("value");
+            fetchData();
+        });
+    });
+
+    const category_buttons = document.querySelectorAll(".category-btn");
+    category_buttons.forEach((button) => {
+        button.addEventListener("mousedown", function () {
+            selected_category = this.getAttribute("value");
+            fetchData();
         });
     });
 });
 
-function difficultyLevelClick() {}
+function difficultyLevelClick() {
+    document.getElementById("difficulty-level-page").style.display = "none";
+    document.getElementById("category-page").style.display = "block";
+}
+
+function fetchData() {
+    if (selected_difficulty && selected_category) {
+        console.log(selected_difficulty, selected_category);
+        let api =
+            "https://opentdb.com/api.php?amount=10&category=" +
+            selected_category +
+            "&difficulty=" +
+            selected_difficulty +
+            "&type=multiple";
+        console.log(api);
+        fetch(api)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    } else {
+        console.error("Both difficulty and category are required");
+    }
+}
