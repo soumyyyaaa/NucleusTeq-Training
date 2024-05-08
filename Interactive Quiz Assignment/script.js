9c33288695a0cd15fcf8007f4070347356c7eec9
soumyyyaaa <soumya.agarwal1103@gmail.com>
2024-05-09 00:52:29 +0530

    Refactored Whole Document

--- Interactive Quiz Assignment/script.js
+++ Interactive Quiz Assignment/script.js
@@ -29,20 +29,28 @@ function difficultyLevelClick() {
     document.getElementById("category-page").style.display = "block";
 }
 
+function categorySubmitClick() {
+    var category_list = document.getElementById("category");
+    selected_category = category_list.value;
+    fetchData();
+}
+
 function fetchData() {
     if (selected_difficulty && selected_category) {
-        console.log(selected_difficulty, selected_category);
         let api =
             "https://opentdb.com/api.php?amount=10&category=" +
             selected_category +
             "&difficulty=" +
             selected_difficulty +
             "&type=multiple";
-        console.log(api);
         fetch(api)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
+                for (let i = 0; i < 10; i++) {
+                    console.log(data.results[i].question);
+                    console.log(data.results[i].incorrect_answers);
+                    console.log(data.results[i].correct_answer);
+                }
             })
             .catch((error) => {
                 console.error("Error:", error);
@@ -50,4 +58,11 @@ function fetchData() {
     } else {
         console.error("Both difficulty and category are required");
     }
+}
+
+var category_center = document.getElementById("category-center");
+if (window.innerWidth >= 480) {
+    category_center.classList.remove("center");
+} else {
+    category_center.classList.add("center");
 }
