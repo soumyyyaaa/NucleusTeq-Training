c08ff9ec58fd5f8cee180609c402519a610f113a
soumyyyaaa <soumya.agarwal1103@gmail.com>
2024-05-11 00:34:17 +0530

    Questions and Answers Functionality Added

--- Interactive Quiz Assignment/script.js
+++ Interactive Quiz Assignment/script.js
@@ -29,22 +29,80 @@ function difficultyLevelClick() {
     document.getElementById("category-page").style.display = "block";
 }
 
+function categoryClick() {
+    document.getElementById("category-page").style.display = "none";
+    document.getElementById("questions").style.display = "block";
+}
+
+let questionIndex = 0;
+let score = 0;
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+
 function fetchData() {
+    const h2 = document.getElementsByClassName("question")[0];
+    const answersDiv = document.getElementsByClassName("answers")[0];
+
     if (selected_difficulty && selected_category) {
-        let api =
+        const api =
             "https://opentdb.com/api.php?amount=10&category=" +
             selected_category +
             "&difficulty=" +
             selected_difficulty +
             "&type=multiple";
+        console.log(api);
         fetch(api)
             .then((response) => response.json())
             .then((data) => {
-                for (let i = 0; i < 10; i++) {
-                    console.log(data.results[i].question);
-                    console.log(data.results[i].incorrect_answers);
-                    console.log(data.results[i].correct_answer);
-                }
+                const question = decodeHtml(
+                    data.results[questionIndex].question
+                );
+                const correctAnswer = decodeHtml(
+                    data.results[questionIndex].correct_answer
+                );
+                let incorrectAnswers = decodeHtml(
+                    data.results[questionIndex].incorrect_answers
+                );
+                incorrectAnswers = incorrectAnswers.split(",");
+                const answers = [correctAnswer].concat(incorrectAnswers);
+                console.log(answers);
+                answers.sort(() => Math.random() - 0.5);
+
+                h2.innerText = question;
+                answersDiv.innerHTML = ""; // Clear previous answers
+                answers.forEach((answer) => {
+                    const button = document.createElement("button");
+                    button.innerText = answer;
+                    button.addEventListener("click", () => {
+                        if (answer === correctAnswer) {
+                            alert("Correct!");
+                            score += 1;
+                        } else {
+                            alert("Incorrect!");
+                        }
+                        // Move to the next question
+                        questionIndex++;
+                        if (questionIndex < 10) {
+                            fetchData(); // Fetch and display the next question
+                        } else {
+                            const restartButton =
+                                document.createElement("button");
+                            restartButton.innerText = "Restart";
+                            restartButton.addEventListener("click", () => {
+                                questionIndex = 0;
+                                score = 0;
+                                fetchData();
+                            });
+                            answersDiv.appendChild(restartButton);
+                        }
+                    });
+                    button.style.display = "block";
+                    answersDiv.appendChild(button);
+                });
             })
             .catch((error) => {
                 console.error("Error:", error);
